1d48a71428331deb04da6e9ca213dc1ba98468ee
marco-giacobbe <marcogiacobbe32@gmail.com>
2025-07-08 17:00:29 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='temperature-prediction',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'torch',
+        # altre librerie...
+    ],
+    author='Marco Giacobbe',
+    description='',
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tuo-username/temperature-prediction-paper',
+    python_requires='>=3.7',
+)
