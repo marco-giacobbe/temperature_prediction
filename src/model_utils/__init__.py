f59b9c416e05228e73f16342afb48d44d275c17a
marco-giacobbe <marcogiacobbe32@gmail.com>
2025-07-13 14:35:32 +0200

    Fixed __all__

--- src/model_utils/__init__.py
+++ src/model_utils/__init__.py
@@ -1,3 +1,3 @@
 from .utils import save_checkpoint, load_checkpoint, train1epoch, eval, evaltf, train, invoke_tflite_interpreter
 
-__all__ = ["save_checkpoint", "load_checkpoint", "train1epoch", "eval", "evaltf", "train"]
+__all__ = ["save_checkpoint", "load_checkpoint", "train1epoch", "eval", "evaltf", "train", "invoke_tflite_interpreter"]
