bc612ae464c3a10004575a48eb6f71db03521293
marco-giacobbe <marcogiacobbe32@gmail.com>
2025-07-14 21:38:28 +0200

    Fixed delta early stop

--- src/model_utils/utils.py
+++ src/model_utils/utils.py
@@ -127,7 +127,7 @@ def train(model, optim, criterion, scheduler, datasets, bsz, out_w, patience, mi
         current_loss = eval(model, criterion, eval_dataset, out_w, False, verbose=verbose)[0]
         eval_losses.append(current_loss)
         epoch += 1
-        if loss_min - current_loss >= 0.002:
+        if loss_min - current_loss >= 0.00005:
             counter = 0
             loss_min = current_loss
             save_checkpoint(model, optim, scheduler, epoch, "./tmp/model_ckpnt.pth")
